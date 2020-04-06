94857cfee29440a912b31ba944929bd4babaf4af
YOON ILLO <yildbs@gmail.com>
2020-04-06 17:55:10 +0900

    * update

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import sys
 from PyQt5.QtWidgets import *
 from PyQt5 import uic, QtCore, QtGui, QtWidgets
-import string
 from ScriptMakingHelper import sentenceanager
 from PyQt5 import uic
-from PyQt5.QtCore import QEvent
+from PyQt5.QtCore import QEvent, QTimer
 from PyQt5.QtWidgets import QApplication, QWidget
 
 main_form_class = uic.loadUiType('Form.ui')[0]
@@ -15,14 +14,14 @@ class Highlighter(QtGui.QSyntaxHighlighter):
         super(Highlighter, self).__init__(parent)
         self.sectionFormat = QtGui.QTextCharFormat()
         self.sectionFormat.setForeground(QtCore.Qt.blue)
-        self.errorFormat = QtGui.QTextCharFormat()
-        self.errorFormat.setForeground(QtCore.Qt.red)
+        self.__highlight_line = 0
 
     def highlightBlock(self, text):
-        if text.startswith('A'):
+        if self.currentBlock().firstLineNumber() == self.__highlight_line:
             self.setFormat(0, len(text), self.sectionFormat)
-        # elif text.startswith('[ERROR]'):
-        #     self.setFormat(0, len(text), self.errorFormat)
+
+    def set_highlight_line(self, value):
+        self.__highlight_line = value
 
 
 class MainWindow(QMainWindow, main_form_class):
@@ -31,54 +30,98 @@ class MainWindow(QMainWindow, main_form_class):
         self.setupUi(self)
         self.setWindowTitle("Script Maker")
 
-        with open('__3_script.txt', 'r', encoding='UTF8') as f:
+        ############################
+        # TODO
+        self.filename = '__3_script.txt'
+
+        ############################
+
+        with open(self.filename, 'r', encoding='UTF8') as f:
             self.scripts = f.readlines()
 
         self.setFocusPolicy(QtCore.Qt.ClickFocus)
 
-        self.manager = sentenceanager.SentenceManager()
-        self.manager.setscript(''.join(self.scripts))
-
         self.highlighter = Highlighter(self.textEdit_script.document())
-        self.textEdit_script.setText(self.manager.sentences)
+
+        self.manager = sentenceanager.SentenceManager()
+        self.manager.set_script(''.join(self.scripts))
+
+        self.set_script()
 
         #Callback & slot
         self.textEdit_script.textChanged.connect(self.textChanged)
+        self.manager.current_line_changed = self.current_line_changed
+        self.manager.sentence_changed = self.set_script
 
         # lineEdit
-        self.lineEdits = []
-        self.lineEdits.append(self.lineEdit_1)
-        self.lineEdits.append(self.lineEdit_2)
-        self.lineEdits.append(self.lineEdit_3)
-        self.lineEdits.append(self.lineEdit_4)
-        self.lineEdits.append(self.lineEdit_5)
-        self.lineEdits.append(self.lineEdit_6)
-        self.lineEdits.append(self.lineEdit_7)
-        self.lineEdits.append(self.lineEdit_8)
-        self.lineEdits.append(self.lineEdit_9)
-        self.lineEdits.append(self.lineEdit_0)
-        self.lineEdits.append(self.lineEdit_q)
-        self.lineEdits.append(self.lineEdit_w)
-        self.lineEdits.append(self.lineEdit_e)
-        self.lineEdits.append(self.lineEdit_r)
-        self.lineEdits.append(self.lineEdit_t)
-        self.lineEdits.append(self.lineEdit_y)
-        self.lineEdits.append(self.lineEdit_u)
-        self.lineEdits.append(self.lineEdit_i)
-        self.lineEdits.append(self.lineEdit_o)
-        self.lineEdits.append(self.lineEdit_p)
+        self.lineEdits = {}
+        self.lineEdits['1'] = self.lineEdit_1
+        self.lineEdits['2'] = self.lineEdit_2
+        self.lineEdits['3'] = self.lineEdit_3
+        self.lineEdits['4'] = self.lineEdit_4
+        self.lineEdits['5'] = self.lineEdit_5
+        self.lineEdits['6'] = self.lineEdit_6
+        self.lineEdits['7'] = self.lineEdit_7
+        self.lineEdits['8'] = self.lineEdit_8
+        self.lineEdits['9'] = self.lineEdit_9
+        self.lineEdits['0'] = self.lineEdit_0
+        self.lineEdits['q'] = self.lineEdit_q
+        self.lineEdits['w'] = self.lineEdit_w
+        self.lineEdits['e'] = self.lineEdit_e
+        self.lineEdits['r'] = self.lineEdit_r
+        self.lineEdits['t'] = self.lineEdit_t
+        self.lineEdits['y'] = self.lineEdit_y
+        self.lineEdits['u'] = self.lineEdit_u
+        self.lineEdits['i'] = self.lineEdit_i
+        self.lineEdits['o'] = self.lineEdit_o
+        self.lineEdits['p'] = self.lineEdit_p
 
         # eventFilter
         self.installEventFilter(self)
         self.textEdit_script.installEventFilter(self)
-        for lineEdit in self.lineEdits:
+        for key, lineEdit in self.lineEdits.items():
             lineEdit.installEventFilter(self)
 
+        # commands
+        self.commands = {}
+        self.commands['a'] = lambda: self.manager.decrement()
+        self.commands['d'] = lambda: self.manager.increment()
+        self.commands['x'] = lambda: self.manager.erase_current_line()
+        self.commands['c'] = lambda: self.manager.clean()
+        for key, lineEdit in self.lineEdits.items():
+            self.commands[key] = (lambda e: (lambda: self.manager.set_who_is_saying(e.text())))(lineEdit)
+
+        try:
+            with open('characters.txt', 'r') as f:
+                lines = []
+                for line in f.readlines():
+                    lines.append(line)
+
+                for line, kv in zip(lines, self.lineEdits.items()):
+                    k, v = kv
+                    v.setText(line.replace('\n', '', len(line)).replace('\r','', len(line)))
+        except FileNotFoundError as e:
+            pass
+
+    def closeEvent(self, QCloseEvent):
+        with open('characters.txt', 'w') as f:
+            for key, lineEdit in self.lineEdits.items():
+                f.write(lineEdit.text() + '\n')
+
+        with open(self.filename, 'w', encoding='UTF8') as f:
+            f.write(self.manager.sentences)
+
+    ########################################
+    # callback functions
     def textChanged(self):
-        self.manager.setscript(self.textEdit_script.toPlainText())
+        self.manager.set_script(self.textEdit_script.toPlainText())
 
     def keyPressEvent(self, p):
-        print(p.key())
+        t = p.text()
+        try:
+            self.commands[t]()
+        except Exception as e:
+            print(e)
 
     def eventFilter(self, obj, event):
         if event.type() == QEvent.FocusIn:
@@ -87,6 +130,15 @@ class MainWindow(QMainWindow, main_form_class):
             else:
                 self.label_focus_state.setText('')
         return super(MainWindow, self).eventFilter(obj, event)
+    # callback functions
+    ########################################
+
+    def set_script(self):
+        self.textEdit_script.setText(self.manager.sentences)
+
+    def current_line_changed(self, n):
+        self.highlighter.set_highlight_line(n)
+        self.highlighter.rehighlight()
 
 
 if __name__ == "__main__":
--- sentenceanager.py
+++ sentenceanager.py
@@ -1,26 +1,74 @@
-
 class SentenceManager:
     def __init__(self):
         self.__sentences = []
+        self.__current_line = 0
 
-    def setscript(self, script):
-        sentences = script.split("\n\r")
+    def set_script(self, script):
+        sentences = script.split("\n")
+        sentences_temp = []
         for sentence in sentences:
-            self.__sentences.append(sentence)
+            sentence = sentence.replace('\n', '', len(sentence)).replace('\r', '', len(sentence))
+            if sentence != '':
+                sentences_temp.append(sentence)
+        self.__sentences = sentences_temp
 
-        for sentence in self.__sentences:
-            print(sentence)
+    @staticmethod
+    def clean_sentence(s):
+        while True:
+            if '[' in s:
+                if ']' in s:
+                    s = s[: s.find('[')] + s[s.find(']') + 1:]
+                else:
+                    s = s.replace('[', '')
+            elif ']' in s:
+                s = s.replace(']', '')
+            else:
+                break
 
+        while True:
+            if s.startswith(' '):
+                s = s[1:]
+            else:
+                break
 
+        for _ in range(10):
+            s = s.replace('  ', ' ', len(s))
+        return s
 
     @property
     def sentences(self):
         return '\n'.join(self.__sentences)
 
+    ###############################
+    # commands
+    ###############################
+    def increment(self):
+        self.__current_line = min(self.__current_line+1, len(self.__sentences)-1)
+        self.current_line_changed(self.__current_line)
 
-        # for sentence in script:
-        #     sentence = sentence.replace('\n', '', len(sentence)).replace('\r', '', len(sentence))
-        #     self.sentences.append(sentence)
+    def decrement(self):
+        self.__current_line = max(self.__current_line - 1, 0)
+        self.current_line_changed(self.__current_line)
+
+    def erase_current_line(self):
+        del self.__sentences[self.__current_line]
+        self.sentence_changed()
+
+    def set_who_is_saying(self, name):
+        if name != '':
+            name = name.replace('\n', '', len(name)).replace('\r', '', len(name))
+            self.__sentences[self.__current_line] = '[%s] ' % name + self.clean_sentence(self.__sentences[self.__current_line])
+            self.sentence_changed()
+
+    def clean(self):
+        self.__sentences[self.__current_line] =  self.clean_sentence(self.__sentences[self.__current_line])
+        self.sentence_changed()
+
+
+    def sentence_changed(self):
+        pass
+    def current_line_changed(self, n):
+        pass
 
 
 
